a460a4b7b1b87a1a0bde912a896992b6aa04015f
Gui Martins <gmartins@fancywhale.ca>
2021-04-23 14:40:14 -0300

    Weeks, Mike: bahaha, you gotta add the exclamation!

--- shared_functions/exceptions.py
+++ shared_functions/exceptions.py
@@ -1,3 +1,3 @@
 class Sorry(Exception):
     def __init__(self, message):
-        self.args = ('Oh hey dere! Sorry bud, but ' + message,)
+        self.args = ('Oh hey dere! Sorry bud, but ' + message + '!',)
